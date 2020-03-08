860103d79afbf3cd157a4f7cf8174f62ef3e972f
Angus <angusjardine@ntlworld>
2020-03-08 13:21:02 +0000

    does this work

--- Map/Doris Tiles.tsx
+++ Map/Doris Tiles.tsx
@@ -1,2 +1,268 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.3" name="MAP TEST" tilewidth="1" tileheight="1" tilecount="0" columns="0"/>
+<tileset version="1.2" tiledversion="1.3.3" name="MAP TEST" tilewidth="64" tileheight="64" tilecount="88" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="53">
+  <image width="64" height="64" source="../Entities/Bottom road #1.1.png"/>
+ </tile>
+ <tile id="54">
+  <image width="64" height="64" source="../Entities/Bottom road #1.2.png"/>
+ </tile>
+ <tile id="55">
+  <image width="64" height="64" source="../Entities/Bottom road #1.png"/>
+ </tile>
+ <tile id="56">
+  <image width="64" height="64" source="../Entities/Bottom road #2.1.png"/>
+ </tile>
+ <tile id="57">
+  <image width="64" height="64" source="../Entities/Bottom road #2.2.png"/>
+ </tile>
+ <tile id="58">
+  <image width="64" height="64" source="../Entities/Bottom road #2.png"/>
+ </tile>
+ <tile id="59">
+  <image width="64" height="64" source="../Entities/Bottom road corner #1.1.png"/>
+ </tile>
+ <tile id="60">
+  <image width="64" height="64" source="../Entities/Bottom road corner #1.2.png"/>
+ </tile>
+ <tile id="61">
+  <image width="64" height="64" source="../Entities/Bottom road corner #1.3.png"/>
+ </tile>
+ <tile id="62">
+  <image width="64" height="64" source="../Entities/Bottom road corner #1.png"/>
+ </tile>
+ <tile id="63">
+  <image width="64" height="64" source="../Entities/bottom road up #1.png"/>
+ </tile>
+ <tile id="64">
+  <image width="64" height="64" source="../Entities/Bridge bottom #1.png"/>
+ </tile>
+ <tile id="65">
+  <image width="64" height="64" source="../Entities/Bridge bottom #2.png"/>
+ </tile>
+ <tile id="66">
+  <image width="64" height="64" source="../Entities/Bridge bottom #3.png"/>
+ </tile>
+ <tile id="67">
+  <image width="64" height="64" source="../Entities/Bridge railing bottom #1.png"/>
+ </tile>
+ <tile id="68">
+  <image width="64" height="64" source="../Entities/Bridge railing bottom #2.png"/>
+ </tile>
+ <tile id="69">
+  <image width="64" height="64" source="../Entities/Bridge railing bottom #3.png"/>
+ </tile>
+ <tile id="70">
+  <image width="64" height="64" source="../Entities/Bridge top #1.png"/>
+ </tile>
+ <tile id="71">
+  <image width="64" height="64" source="../Entities/Bridge top #2.png"/>
+ </tile>
+ <tile id="72">
+  <image width="64" height="64" source="../Entities/Bridge top #3.png"/>
+ </tile>
+ <tile id="73">
+  <image width="64" height="64" source="../Entities/Cave Entrance left side.png"/>
+ </tile>
+ <tile id="74">
+  <image width="64" height="64" source="../Entities/Cave Entrance.png"/>
+ </tile>
+ <tile id="75">
+  <image width="64" height="64" source="../Entities/Dirt tile #1.png"/>
+ </tile>
+ <tile id="76">
+  <image width="64" height="64" source="../Entities/Dirt tile #2.png"/>
+ </tile>
+ <tile id="77">
+  <image width="64" height="64" source="../Entities/Dirt tile #3.png"/>
+ </tile>
+ <tile id="78">
+  <image width="64" height="64" source="../Entities/Fire.gif"/>
+ </tile>
+ <tile id="79">
+  <image width="64" height="64" source="../Entities/Grass blades #1.png"/>
+ </tile>
+ <tile id="80">
+  <image width="64" height="64" source="../Entities/Grass blades #2.png"/>
+ </tile>
+ <tile id="81">
+  <image width="64" height="64" source="../Entities/Grass blades #3.png"/>
+ </tile>
+ <tile id="82">
+  <image width="64" height="64" source="../Entities/Grass blades #4.png"/>
+ </tile>
+ <tile id="83">
+  <image width="64" height="64" source="../Entities/Grass blades #5.png"/>
+ </tile>
+ <tile id="84">
+  <image width="64" height="64" source="../Entities/Grass blades #6.png"/>
+ </tile>
+ <tile id="85">
+  <image width="64" height="64" source="../Entities/Grass blades #7.png"/>
+ </tile>
+ <tile id="86">
+  <image width="64" height="64" source="../Entities/Grass Bush #1.png"/>
+ </tile>
+ <tile id="87">
+  <image width="64" height="64" source="../Entities/Grass Bush #2.png"/>
+ </tile>
+ <tile id="88">
+  <image width="64" height="64" source="../Entities/Grass tile #1.png"/>
+ </tile>
+ <tile id="89">
+  <image width="64" height="64" source="../Entities/Grass tile #2.png"/>
+ </tile>
+ <tile id="90">
+  <image width="64" height="64" source="../Entities/Grass tile #3.png"/>
+ </tile>
+ <tile id="91">
+  <image width="64" height="64" source="../Entities/Grass tile #5.png"/>
+ </tile>
+ <tile id="92">
+  <image width="64" height="64" source="../Entities/Grass tile #6.png"/>
+ </tile>
+ <tile id="93">
+  <image width="64" height="64" source="../Entities/Leafs #1.png"/>
+ </tile>
+ <tile id="94">
+  <image width="64" height="64" source="../Entities/Leafs #2.png"/>
+ </tile>
+ <tile id="95">
+  <image width="64" height="64" source="../Entities/Log Fireplace.png"/>
+ </tile>
+ <tile id="96">
+  <image width="64" height="64" source="../Entities/River animation.gif"/>
+ </tile>
+ <tile id="97">
+  <image width="64" height="64" source="../Entities/River bank animation.gif"/>
+ </tile>
+ <tile id="98">
+  <image width="64" height="64" source="../Entities/River bank LEFT side #1.png"/>
+ </tile>
+ <tile id="99">
+  <image width="64" height="64" source="../Entities/River bank LEFT side #2.png"/>
+ </tile>
+ <tile id="100">
+  <image width="64" height="64" source="../Entities/River bank LEFT side #3.png"/>
+ </tile>
+ <tile id="101">
+  <image width="64" height="64" source="../Entities/River LEFT corner #1.1.png"/>
+ </tile>
+ <tile id="102">
+  <image width="64" height="64" source="../Entities/River LEFT corner #1.2.png"/>
+ </tile>
+ <tile id="103">
+  <image width="64" height="64" source="../Entities/River LEFT corner #1.3.png"/>
+ </tile>
+ <tile id="104">
+  <image width="64" height="64" source="../Entities/River LEFT corner #1.png"/>
+ </tile>
+ <tile id="105">
+  <image width="64" height="64" source="../Entities/River LEFT side #1.png"/>
+ </tile>
+ <tile id="106">
+  <image width="64" height="64" source="../Entities/River LEFT side #2.1.png"/>
+ </tile>
+ <tile id="107">
+  <image width="64" height="64" source="../Entities/River LEFT side #2.2.png"/>
+ </tile>
+ <tile id="108">
+  <image width="64" height="64" source="../Entities/River LEFT side #2.3.png"/>
+ </tile>
+ <tile id="109">
+  <image width="64" height="64" source="../Entities/River LEFT side #2.png"/>
+ </tile>
+ <tile id="110">
+  <image width="64" height="64" source="../Entities/River LEFT side #3.1png.png"/>
+ </tile>
+ <tile id="111">
+  <image width="64" height="64" source="../Entities/River LEFT side #3.2png.png"/>
+ </tile>
+ <tile id="112">
+  <image width="64" height="64" source="../Entities/River LEFT side #3.3png.png"/>
+ </tile>
+ <tile id="113">
+  <image width="64" height="64" source="../Entities/River LEFT side #3.png"/>
+ </tile>
+ <tile id="114">
+  <image width="64" height="64" source="../Entities/River RIGHT corner #1.1.png"/>
+ </tile>
+ <tile id="115">
+  <image width="64" height="64" source="../Entities/River RIGHT corner #1.2.png"/>
+ </tile>
+ <tile id="116">
+  <image width="64" height="64" source="../Entities/River RIGHT corner #1.3.png"/>
+ </tile>
+ <tile id="117">
+  <image width="64" height="64" source="../Entities/River RIGHT corner #1.png"/>
+ </tile>
+ <tile id="118">
+  <image width="64" height="64" source="../Entities/Rock #1.png"/>
+ </tile>
+ <tile id="119">
+  <image width="64" height="64" source="../Entities/Tent #1.png"/>
+ </tile>
+ <tile id="120">
+  <image width="64" height="64" source="../Entities/Tent #2.png"/>
+ </tile>
+ <tile id="121">
+  <image width="64" height="64" source="../Entities/Top bridge railing #1.png"/>
+ </tile>
+ <tile id="122">
+  <image width="64" height="64" source="../Entities/Top bridge railing #2.png"/>
+ </tile>
+ <tile id="123">
+  <image width="64" height="64" source="../Entities/Top bridge railing #3.png"/>
+ </tile>
+ <tile id="124">
+  <image width="64" height="64" source="../Entities/Top road #1.1png.png"/>
+ </tile>
+ <tile id="125">
+  <image width="64" height="64" source="../Entities/Top road #1.png"/>
+ </tile>
+ <tile id="126">
+  <image width="64" height="64" source="../Entities/Top road #2.2.png"/>
+ </tile>
+ <tile id="127">
+  <image width="64" height="64" source="../Entities/Top road #2.png"/>
+ </tile>
+ <tile id="128">
+  <image width="64" height="64" source="../Entities/Top road corner #2.1.png"/>
+ </tile>
+ <tile id="129">
+  <image width="64" height="64" source="../Entities/Top road corner #2.2.png"/>
+ </tile>
+ <tile id="130">
+  <image width="64" height="64" source="../Entities/Top road corner #2.3.png"/>
+ </tile>
+ <tile id="131">
+  <image width="64" height="64" source="../Entities/Top road corner #2.png"/>
+ </tile>
+ <tile id="132">
+  <image width="64" height="64" source="../Entities/Tree head #1.png"/>
+ </tile>
+ <tile id="133">
+  <image width="64" height="64" source="../Entities/Tree head #2.png"/>
+ </tile>
+ <tile id="134">
+  <image width="64" height="64" source="../Entities/Tree Stump #1.png"/>
+ </tile>
+ <tile id="135">
+  <image width="64" height="64" source="../Entities/Tree Stump #2.png"/>
+ </tile>
+ <tile id="136">
+  <image width="64" height="64" source="../Entities/Water pool #1 WATER.png"/>
+ </tile>
+ <tile id="137">
+  <image width="64" height="64" source="../Entities/Water pool #1.png"/>
+ </tile>
+ <tile id="138">
+  <image width="64" height="64" source="../Entities/Grass Patches #1.png"/>
+ </tile>
+ <tile id="139">
+  <image width="64" height="64" source="../Entities/Grass Patches #2.png"/>
+ </tile>
+ <tile id="140">
+  <image width="64" height="64" source="../Entities/River Deep Water #1.png"/>
+ </tile>
+</tileset>
