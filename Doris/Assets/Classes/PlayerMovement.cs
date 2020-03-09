730a9562bf263430395a952c6755bd45e1de475e
Richard Belovic <r.belovic@lancaster.ac.uk>
2020-03-09 18:04:52 +0000

    Added collisions, player animation, player movement, player can go behind shit

---
+++ Doris/Assets/Classes/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    private Animator anim;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        anim.SetFloat("Vertical", Input.GetAxis("Vertical"));
+        anim.SetFloat("Magnitude", Mathf.Abs(Input.GetAxis("Vertical") + Input.GetAxis("Horizontal")));
+
+        rb.velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal")* speed, 1f), Mathf.Lerp(0, Input.GetAxis("Vertical")* speed, 1f));
+    }
+}
